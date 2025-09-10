50da27e6a126a59fdccb11559a5a197c8aa8cc4c
Alex Barsukov <mozdoksity@ya.ru>
2025-09-10 14:50:59 +0300

    new

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,11 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	
+
+	@Test
+	public void welcomerSaysHunterHunt(){
+		assertThat(welcomer.sayHunterHunt(), containsString("hunter"));
+	}
+
 }
